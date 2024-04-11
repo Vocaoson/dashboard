fccb79340b9a703795a028539de55883c990f81b
Bruce <vocaoson7@gmail.com>
2024-04-11 15:55:07 +0700

    add GA

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,13 @@
+import { GoogleAnalytics, sendGAEvent } from '@next/third-parties/google';
 import React from 'react';
 
 const Page = () => {
   return (
-    <div>Customer page</div>
+    <div>
+
+      <div>Customer page</div>
+      <GoogleAnalytics gaId="G-XQ1ZMK8FFV" />
+    </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,22 @@
+'use client';
 import '@/app/ui/global.css';
 import { inter, lusitana } from './ui/fonts';
+import { GoogleAnalytics, sendGAEvent } from '@next/third-parties/google'
+import { useEffect } from 'react';
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    sendGAEvent('EnterViaQR', {product_id: '1234'});
+  }, []);
+
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
+      <GoogleAnalytics gaId="G-XQ1ZMK8FFV" />
     </html>
   );
+
 }
